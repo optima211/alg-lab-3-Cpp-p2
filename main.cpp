3a94040af9c2410cddc5f59784fccef5c86a8d57
optima211 <nedmk@mail.ru>
2018-12-21 22:56:27 +0700

    add based function (good work when size = 11)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,192 @@
-#include <iostream>
+//---------------------------------------------------------------------------
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#pragma hdrstop
+
+#include <conio.h>
+#include <iostream>
+#include <math.h>
+#include <string.h>
+
+using namespace std;
+//---------------------------------------------------------------------------
+// Выводит строку s в консоль русскими буквами
+//void PrintRus(char *s)
+//{
+//    char s1[100];
+//    CharToOem(s, s1); // Делаем перекодировку
+//    cout << s1;
+//}
+
+int m; // Размер хеш-таблицы
+int table[100]; // Хеш-таблица
+int n; // Размер массива чисел
+int a[100]; // Массив чисел
+bool overflow; // Равна true, если было переполнение таблицы
+int collisions; // Количество коллизий
+
+// Вычисляет хеш-функцию
+int HashF(int num) {
+    return num % m;
+}
+
+// Создает хеш-таблицу методом линейных проб
+void CreateTable1() {
+    int i;
+    for (i = 0; i < m; i++) // Очищаем хэш-таблицу
+        table[i] = 0;
+    collisions = 0;
+    int h, h0, g;
+    for (i = 0; i < n; i++) {
+        h = HashF(a[i]);
+        h0 = h;
+        g = 1;
+        while (1) {
+            if (table[h] == a[i]) // Если число повторяется
+                break;
+            if (table[h] == 0) { // Если нашли пустое место в хэш-таблице
+                table[h] = a[i];
+                break;
+            }
+            if (g >= m) { // Если переполнение
+                overflow = true;
+                return;
+            }
+            collisions++;
+            h = h0 + g;
+            if (h >= m)
+                h -= m;
+            g++;
+        }
+    }
+    overflow = false;
+}
+
+// Создает хеш-таблицу методом квадратичных проб
+void CreateTable2() {
+    int i;
+    for (i = 0; i < m; i++) // Очищаем хэш-таблицу
+        table[i] = 0;
+    collisions = 0;
+    int h, d;
+    for (i = 0; i < n; i++) {
+        h = HashF(a[i]);
+        d = 1;
+        while (1) {
+            if (table[h] == a[i]) // Если число повторяется
+                break;
+            if (table[h] == 0) { // Если нашли пустое место в хэш-таблице
+                table[h] = a[i];
+                break;
+            }
+            if (d >= m) { // Если переполнение
+                overflow = true;
+                return;
+            }
+            collisions++;
+            h += d;
+            if (h >= m)
+                h -= m;
+            d += 2;
+        }
+    }
+    overflow = false;
+}
+
+// Выводит хеш-таблицу на экран
+void ShowTable() {
+    if (m == 11) {
+        printf("-----------------------------------------------------------------------\n");
+        printf("| cell number |  0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 |\n");
+        printf("-----------------------------------------------------------------------\n");
+        printf("| Number      | ");
+        for (int i = 0; i < m; i++)
+            if (table[i] != 0)
+                printf("%2d | ", table[i]);
+            else
+                printf("   | ");
+        cout << endl;
+        printf("-----------------------------------------------------------------------\n");
+    }
+    if (!overflow)
+        printf("\nThere was no table overflow\n");
+    else
+        printf("\nAn overflow occurred in the table\n");
+    printf("\nNumber of collisions: \n");
+    cout << collisions << endl;
+}
+
+// Ищет число в хеш-таблице, построенной методом линейных проб
+bool Search1(int num) {
+    int h, h0, g;
+    h = HashF(num);
+    h0 = h;
+    g = 1;
+    while (1) {
+        if (table[h] == num) // Если число найдено
+            return true;
+        if (g >= m) // Если переполнение
+            return false;
+        h = h0 + g;
+        if (h >= m)
+            h -= m;
+        g++;
+    }
+}
+
+// Ищет число в хеш-таблице, построенной методом квадратичных проб
+bool Search2(int num) {
+    int h, d;
+    h = HashF(num);
+    d = 1;
+    while (1) {
+        if (table[h] == num) // Если число найдено
+            return true;
+        if (d >= m) // Если переполнение
+            return false;
+        h += d;
+        if (h >= m)
+            h -= m;
+        d += 2;
+    }
+}
+
+#pragma argsused
+
+int main(int argc, char *argv[]) {
+    printf("\nEnter the size of the hash table: \n");
+    cin >> m;
+    printf("\nEnter the number of source numbers: \n");
+    cin >> n;
+
+    int num; // Число для поиска
+
+//    rand();
+    int i;
+    for (i = 0; i < n; i++) // Заполняем массив чисел
+        a[i] = rand() % n + 1;
+
+    printf("\nLinear sampling method:\n");
+    CreateTable1();
+    ShowTable();
+    printf("\nEnter a number to search: \n");
+    cin >> num;
+    if (Search1(num))
+        printf("\nThe number of found\n");
+    else
+        printf("\nThe number is not found\n");
+
+    printf("\nQuadratic sample method:\n");
+    CreateTable2();
+    ShowTable();
+    printf("\nEnter a number to search: \n");
+    cin >> num;
+    if (Search2(num))
+        printf("\nThe number of found\n");
+    else
+        printf("\nThe number is not found\n");
+
+    getch();
     return 0;
 }
+//---------------------------------------------------------------------------
+ 
